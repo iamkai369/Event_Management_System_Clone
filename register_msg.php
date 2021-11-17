5bafc3c0600371fb9d9c803b00da76260d23a2f6
iamkai369 <86986813+iamkai369@users.noreply.github.com>
2021-11-17 16:19:25 +0300

    Add files via upload

---
+++ register_msg.php
@@ -0,0 +1,29 @@
+<div class="container-fluid">
+	<p>Thank you for registering for this event! Please check your email for verification on your booking <strong>OR</strong> </p>
+</div>
+<div class="modal-footer display">
+	<div class="row">
+		<div class="col-md-12">
+			<button class="btn btn-secondary float-right" type="button" data-dismiss="modal">Close</button>
+			
+			<!-- revisit code to adjust target="_blank" --> 
+
+			<button onclick="location.href = 'https://twitter.com/ ';" id="myButton" class="float-left submit-button">Proceed to Payment</button>
+		</div>
+
+		<!--<div class="col-md-12">
+			<button class="btn btn-secondary float-right" type="button" data-dismiss="modal">Proceed to Payment <a href="www.payment.com"></a></button>
+		</div> -->
+
+	</div>
+</div>
+<style>
+	#uni_modal .modal-footer{
+		display: none;
+	}
+	#uni_modal .modal-footer.display{
+		display: block;
+	}
+</style>
+
+
